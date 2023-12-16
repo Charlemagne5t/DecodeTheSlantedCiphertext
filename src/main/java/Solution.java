82a182eed2bcbed1ce23ae41950fc08f1c1101ad
alek <wait82985@gmail.com>
2023-12-16 18:25:06 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,15 @@
 public class Solution {
     public String decodeCiphertext(String encodedText, int rows) {
-        return  null;
-
+        int columns = encodedText.length() / rows;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < columns; i++) {
+            int start = i;
+            while (start < encodedText.length()) {
+                sb.append(encodedText.charAt(start));
+                start += columns + 1;
+            }
+        }
+       String answer = sb.toString().stripTrailing();
+        return answer;
     }
 }
